df54baaa88dce45c661ca1699076356eb1902110
tsaharya <tarunsaharya@gmail.com>
2023-08-24 17:15:26 -0400

    finished draft of rust

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod container_most_water;
 mod search_2d_matrix;
 
 fn main() {
-    let matrix = vec![vec![2, 3, 4], vec![2, 4, 3], vec![4, 2, 3], vec![3, 2, 4]];
-    let target = 8;
-    search_2d_matrix::search_2d_matrix(matrix, target);
+    let matrix = vec![vec![1]];
+    let target = 0;
+    let result = search_2d_matrix::search_matrix(matrix, target);
     println!("The result is: {result}");
 }
--- src/search_2d_matrix/mod.rs
+++ src/search_2d_matrix/mod.rs
@@ -5,23 +5,21 @@ fn get_element_from_idx(matrix: &Vec<Vec<i32>>, mid_idx: usize, num_rows: usize)
     return matrix[row][col];
 }
 
-pub fn search_2d_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
     let num_rows = matrix.len();
-    let num_cols = matrix.first().len();
+    let num_cols = matrix.first().unwrap().len();
     let mut left = 0;
     let mut right = (num_rows * num_cols) - 1;
 
     while left <= right {
-        mid_idx = (left + right) / 2;
-        mid_element = get_elem_from_idx(&matrix, mid_idx, num_rows);
+        let mid_idx = (left + right) / 2;
+        let mid_element = get_element_from_idx(&matrix, mid_idx, num_rows);
 
         if mid_element == target {
             return true;
-        }
-
-        if mid_element < target {
+        } else if mid_element < target {
             left = mid_idx + 1;
-        } else {
+        } else if mid_element > target {
             right = mid_idx - 1;
         }
     }
