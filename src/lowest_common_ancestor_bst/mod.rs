38a2af6ffbe3812573e1433be473c7c116aa5436
tsaharya <tarunsaharya@gmail.com>
2023-09-21 17:57:07 -0400

    committed so far

--- src/lowest_common_ancestor_bst/mod.rs
+++ src/lowest_common_ancestor_bst/mod.rs
@@ -1,21 +1,26 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
+use self::tree_node::TreeNode;
+mod tree_node {
+    use std::{cell::RefCell, rc::Rc};
 
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
+    // Definition for a binary tree node.
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct TreeNode {
+        pub val: i32,
+        pub left: Option<Rc<RefCell<TreeNode>>>,
+        pub right: Option<Rc<RefCell<TreeNode>>>,
+    }
+
+    impl TreeNode {
+        #[inline]
+        pub fn new(val: i32) -> Self {
+            TreeNode {
+                val,
+                left: None,
+                right: None,
+            }
         }
     }
 }
@@ -25,8 +30,8 @@ pub fn lowest_common_ancestor(
     p: Option<Rc<RefCell<TreeNode>>>,
     q: Option<Rc<RefCell<TreeNode>>>,
 ) -> Option<Rc<RefCell<TreeNode>>> {
-    let cursor: Rc<RefCell<TreeNode>> = root.unwrap();
-cursor.replace(t)
+    let mut cur_root: Rc<RefCell<TreeNode>> = root.unwrap();
+
     let p: Rc<RefCell<TreeNode>> = p.unwrap();
     let q: Rc<RefCell<TreeNode>> = q.unwrap();
 
@@ -34,23 +39,18 @@ cursor.replace(t)
     let q_val = q.borrow().val;
 
     loop {
-        let cursor_val = cursor.borrow().val;
+        let underlying = cur_root.borrow();
+        let mut cur_root_val = underlying.val;
 
         // go left if both less than current spot
-        if p_val < cursor_val && q_val < cursor_val {
-            let new_root = Rc::clone(cursor.borrow().left.as_ref().unwrap());
-            
+        if p_val < cur_root_val && q_val < cur_root_val {
+            cur_root = Rc::clone(underlying.left.as_ref().unwrap());
+
         // go right if both higher than current spot
-        } else if p_val > cursor_val && q_val > cursor_val {
-            let new_root = Rc::clone(cursor.borrow().right.as_ref().unwrap());
+        } else if p_val > cur_root_val && q_val > cur_root_val {
+            cur_root = Rc::clone(underlying.right.as_ref().unwrap());
         } else {
-            return Some(cursor);
+            return Some(cur_root);
         }
     }
-
-    // ---------------------------
-
-    //let l = cursor.replace(Rc::clone(&cursor.borrow().left));
-
-    unreachable!();
 }
