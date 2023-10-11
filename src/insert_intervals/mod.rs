71f8a61af4105791e46f70c7c7057f765ec71f7d
tsaharya <tarunsaharya@gmail.com>
2023-10-10 21:31:58 -0400

    pushing some stuff

---
+++ src/insert_intervals/mod.rs
@@ -0,0 +1,1 @@
+pub fn insert(intervals: Vec<Vec<i32>>, new_interval: Vec<i32>) -> Vec<Vec<i32>> {}
--- src/minimum_rotated_array/solution.py
+++ src/minimum_rotated_array/solution.py
@@ -1,22 +1,18 @@
-nums = [3,4,5,1,2]
-#Output: 1
+nums = [3, 4, 5, 1, 2]
+# Output: 1
 
 
-def min_finder(lst: list)->int:
+def min_finder(lst: list) -> int:
     l = 0
     r = len(lst)
-    previous = mid - 1 
+    mid = (l + r) // 2
+    previous = mid - 1
 
     while True:
-        mid = (l + r)//2
-        if lst[previous] > lst[mid]: 
+        mid = (l + r) // 2
+        if lst[previous] > lst[mid]:
             return lst[mid]
         if lst[mid] > lst[r]:
             l = mid + 1
-        else: 
+        else:
             r = mid - 1
-        
-        
-
-
-        
